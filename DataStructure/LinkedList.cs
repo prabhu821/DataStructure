5647c290d242eee0fafd852acedd867ed56653af
rishiraj007 <rishiraj@mounttalent.com>
2022-12-13 18:56:35 +0530

    [Prabhu]: Create Linked List

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class LinkedList
+    {
+        //ADD DATA
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head==null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next !=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+      
+
+
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LinkedList list = new LinkedList();
+            Console.WriteLine("Welcome to the Linked List Operations");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect the operation to perform \n1.Create Linked List \n2.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        break;
+                    
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
